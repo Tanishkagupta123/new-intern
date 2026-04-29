111825398529d14f0e84f02d7841700a79c6ca55
Tanishkagupta123 <tanishkagupta241@gmail.com>
2026-04-29 23:54:17 +0530

    first commit

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('todos/',todo_list),
+    path('todos/<int:pk>/',todo_detail),
 ]
